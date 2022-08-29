6ffe2f4897442d4540e88e0cab9e4fc3c799146b
JermaineSegun <olusegunakinlolu@gmail.com>
2022-08-29 02:18:20 -0700

    change table line

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,5 +9,5 @@
 
 """ program removes all characters c and C from a string """
 def no_c(my_string):
-    table = my_string.translate({ord('c'): '', ord('C'): ''})
+    table = my_string.translate({ord(c): None for c in "cC"})
     return(table)
