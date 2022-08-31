a4b4d311f38f235438259198023c0e4705b7e8f4
JermaineSegun <olusegunakinlolu@gmail.com>
2022-08-31 09:31:49 -0700

    add if statement

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,8 @@
 """ function computes the square value of all integers in a matrix """
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for x in matrix:
-        new_matrix.append(list(map(lambda x: x ** 2, x)))
-    return new_matrix
+    if matrix is not None:
+        new_matrix = []
+        for x in matrix:
+            new_matrix.append(list(map(lambda x: x ** 2, x)))
+        return new_matrix
