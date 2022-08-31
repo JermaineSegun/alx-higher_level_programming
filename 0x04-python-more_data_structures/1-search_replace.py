b31cb3ce16595465ea8fda71e6ea3770db930105
JermaineSegun <olusegunakinlolu@gmail.com>
2022-08-31 10:14:52 -0700

    Write a function that replaces all occurrences of an element by another in a new list.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-""" function replaces all occurences ofan element by another in a new list """
-
 def search_replace(my_list, search, replace):
-    return(list(map(lambda x: replace if x is search else x, my_list)))
+    if my_list is not None:
+        return [replace if item is search else item for item in my_list]
