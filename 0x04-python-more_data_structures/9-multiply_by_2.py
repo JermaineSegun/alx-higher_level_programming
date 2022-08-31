c245093615c98dc5e170cdde7fd04e08bee485c2
JermaineSegun <olusegunakinlolu@gmail.com>
2022-08-31 13:22:40 -0700

    add if statement

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,8 @@
 """ function returns a new dictionary with all values multiplied by 2 """
 
 def multiply_by_2(a_dictionary):
-    tmp_dict = a_dictionary.copy()
-    for x in tmp_dict.keys():
-        tmp_dict[x] *= 2
-    return tmp_dict
+    if a_dictionary is not None:
+        tmp_dict = a_dictionary.copy()
+        for x in tmp_dict.keys():
+            tmp_dict[x] *= 2
+        return tmp_dict
